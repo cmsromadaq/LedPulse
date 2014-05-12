1fafa52ba3c33e3d3a5de50342879c365f68814f
CMS Roma DAQ user <cmsroma.daq@gmail.com>
2014-05-12 06:39:57 +0200

    Adding serial communication

--- LedPulse.ino
+++ LedPulse.ino
@@ -11,8 +11,10 @@ int led = 13;
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
+      Serial.begin(9600);       
+// initialize the digital pin as an output.
   pinMode(led, OUTPUT);     
+  
 }
 
 // the loop routine runs over and over again forever:
